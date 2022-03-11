a38ca44e39194dc3b1c3cfabc4a04003706cbe0d
mikhail-cct <tayemnyj+cct@gmail.com>
2022-03-11 14:02:16 +0000

    wk4 films html & ts to intro 3 buttons + colour

--- mobdev-app/src/app/pages/films/films.page.ts
+++ mobdev-app/src/app/pages/films/films.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-films',
@@ -6,10 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./films.page.scss'],
 })
 export class FilmsPage implements OnInit {
-
-  constructor() { }
-
+ 
+  constructor(private navController: NavController, private router: Router) { }
+ 
   ngOnInit() {
   }
-
+ 
+  openDetails() {
+    // Both of these would work!
+    // But the standard Router is recommended.
+    // this.navController.navigateForward(`/tabs/films/42`);
+    this.router.navigateByUrl(`/tabs/films/42`);
+  }
+ 
+  goToPlanets() {
+    this.navController.navigateRoot(`/tabs/planets`)
+  }
+ 
 }
